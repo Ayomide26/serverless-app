f77f1f5aff3c961075bf2fdd2de765832b093b60
Ayomide26 <ayomidefadare26@gmail.com>
2022-12-01 19:42:21 +0100

    first commit

---
+++ serveless-todo-app1/backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,37 @@
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+import { deleteTodo } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+    const userId = getUserId(event)
+
+    // TODO: Remove a TODO item by id
+    // const authorization = event.headers.Authorization;
+    // const split = authorization.split(' ');
+    // const jwtToken = split[1];
+  
+  await deleteTodo(todoId, userId)
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({body: ""})
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
